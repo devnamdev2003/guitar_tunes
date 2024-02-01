335035a8f309f06f32b72b29dd265030c9997526
Dev Namdev <devnamdevcse@gmail.com>
2024-02-01 21:13:02 +0530

    add

--- scripts/script.js
+++ scripts/script.js
@@ -11,24 +11,24 @@ new Vue({
       tracks: [
         {
           name: "Kaise hua",
-          cover: "https://github.com/devnamdev2003/guitar_tunes/raw/main/img/d1.jpg",
-          source: "https://github.com/devnamdev2003/guitar_tunes/raw/main/mp3/kaise_hua.mp4",
+          cover: "https://devnamdev2003.github.io/guitar_tunes/img/d1.jpg",
+          source: "https://devnamdev2003.github.io/guitar_tunes/mp3/kaise_hua.mp4",
           favorited: false
         }, {
           name: "I love you bodyguard ",
-          cover: "https://github.com/devnamdev2003/guitar_tunes/raw/main/img/d2.jpg",
-          source: "https://github.com/devnamdev2003/guitar_tunes/raw/main/mp3/i_love_you.mp4",
+          cover: "https://devnamdev2003.github.io/guitar_tunes/img/d2.jpg",
+          source: "https://devnamdev2003.github.io/guitar_tunes/mp3/i_love_you.mp4",
           favorited: false
         }, {
           name: "Dekha Hazaro Dafaa",
-          cover: "https://github.com/devnamdev2003/guitar_tunes/raw/main/img/d3.jpg",
-          source: "https://github.com/devnamdev2003/guitar_tunes/raw/main/mp3/Dekha_Hazaro_Dafaa.mp4",
+          cover: "https://devnamdev2003.github.io/guitar_tunes/img/d3.jpg",
+          source: "https://devnamdev2003.github.io/guitar_tunes/mp3/Dekha_Hazaro_Dafaa.mp4",
           favorited: false
         }, {
           name: "Pehla Pyaar Hai",
           // artist: "dev",
-          cover: "https://github.com/devnamdev2003/guitar_tunes/raw/main/img/d5.jpg",
-          source: "https://github.com/devnamdev2003/guitar_tunes/raw/main/mp3/Pehla_Pyaar_Hai.mp4",
+          cover: "https://devnamdev2003.github.io/guitar_tunes/img/d5.jpg",
+          source: "https://devnamdev2003.github.io/guitar_tunes/mp3/Pehla_Pyaar_Hai.mp4",
           favorited: false
         },
       ],
